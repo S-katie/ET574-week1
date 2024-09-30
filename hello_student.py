3d42009adc84f175d604323f2b8005176be187cf
S-katie <soe.kaythi38@student.qcc.cuny.edu>
2024-09-30 03:40:31 +0000

    add input for student

--- hello_student.py
+++ hello_student.py
@@ -1,2 +1,7 @@
 #use print() function
-print('hi student from ET574!')
+print('hi student from ET574!\n')
+try:
+    courseNum = eval(input("Enter your course number: "))
+    print(f"\nHello, welcome to the course ET{courseNum}!")
+except:
+    print("Hello, world!")
